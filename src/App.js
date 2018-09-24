f3ea58083d4c2d89ae786f5bccc5e522494cd84d
Momo Rao <monicaxrao@gmail.com>
2018-09-24 16:32:04 -0400

    feast: Add google map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
+/* global google */
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as MapStyle from './MapStyle'
+import Panel from './Panel'
 
 class App extends Component {
+  getGoogleMaps() {
+    if (!this.googleMapsPromise) {
+      this.googleMapsPromise = new Promise((resolve) => {
+        window.resolveGoogleMapsPromise = () => {
+          resolve(google);
+        };
+        const script = document.createElement("script");
+        const API = 'AIzaSyDiADYLnih_NW88akZPOpqVTZIjLbv_8Zo';
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${API}&callback=resolveGoogleMapsPromise`;
+        script.async = true;
+        document.body.appendChild(script);
+      });
+    }
+    return this.googleMapsPromise;
+  }
+
+  componentWillMount() {
+    this.getGoogleMaps();
+  }
+
+  componentDidMount() {
+    this.getGoogleMaps().then((google) => {
+      const city = {lat:38.018175, lng:-78.498115};
+      const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: city,
+        styles: MapStyle.style
+      });
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App"> 
+        <Panel />   
+        <div id="map"></div>
       </div>
     );
   }
---
+++ src/Panel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Panel.css'
+
+class Panel extends Component {
+	render() {
+		return (
+			<div className="panel">
+				<h1 className="title">Foodie Map</h1>
+			</div>
+		);
+	}
+}
+export default Panel;
